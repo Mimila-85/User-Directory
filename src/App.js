bef26c78576b8721852eb4025affbf3f88d60e4d
Camila Alves Meyer <camila.alves85@gmail.com>
2021-02-04 20:25:23 -0800

    Created App.js file

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './App.css';
+import EmployeeContainer from "./components/EmployeeContainer"
+
+function App() {
+  return (
+    <EmployeeContainer />
+  );
+}
+
+export default App;

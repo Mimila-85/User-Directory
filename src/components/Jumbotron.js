e730a03fa4d724b3b3127372570ad7ce8f7340fb
Camila Alves Meyer <camila.alves85@gmail.com>
2021-02-04 20:20:24 -0800

    Created jumbotron component

---
+++ src/components/Jumbotron.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Jumbotron(pros) {
+    return(
+    <div>
+        <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+          <h1 className="display-4">{pros.heading}</h1>
+          <p className="lead">{pros.subHeading}</p>
+        </div>
+      </div>
+    </div>
+    );
+}
+
+export default Jumbotron;
